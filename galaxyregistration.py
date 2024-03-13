71deb154bb4cafd4f69b0a95ca4d3194afea4cb6
Your Name <you@example.com>
2024-03-13 16:56:39 -0300

    Project 01

---
+++ galaxyregistration.py
@@ -0,0 +1,4 @@
+print('Welcome to the Galaxy Registration Department')
+name = input('What is your last name? ')
+birth = input('What is your birth year? ')
+print("Galaxy Name: " + (name) + (birth))
